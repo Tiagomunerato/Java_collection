0c04d2fcc8fb5e9e622672fc511d1aa39bb4a473
unknown <tiagomunerato@gmail.com>
2021-08-28 10:59:57 -0300

    :tada:

---
+++ src/String_funcoes.java
@@ -0,0 +1,22 @@
+
+public class String_funcoes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String original = "abcd GHGHG HJHJ abcc JKJK";
+		
+		String s1 = original.toLowerCase();
+		
+		String s2 = original.toUpperCase();
+		
+		String s3 = original.trim();
+
+		System.out.println(s1+s3);
+		
+		System.out.println(s2);
+		
+
+	}
+
+}
